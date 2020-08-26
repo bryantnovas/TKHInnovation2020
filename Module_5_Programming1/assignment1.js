253f88e4570c3e46c594048af737a5a9970e353a
Bryant Novas <bryant.novas@gmail.com>
2020-08-26 01:32:45 -0400

    fixes fraction issue

--- Module_5_Programming1/assignment1.js
+++ Module_5_Programming1/assignment1.js
@@ -3,14 +3,14 @@ const fCost = parseInt(prompt("weekly food cost?"));
 const hCost = parseInt(prompt("weekly housing cost?"));
 const tCost = parseInt(prompt("weekly transportation cost?"));
 const oCost = parseInt(prompt("other weekly cost?"));
-const savings = parseInt(prompt("how much you wanto to save in a year?"));
+const savings = parseInt(prompt("how much you want to save in a year?"));
 const weeklySavings = savings / 52;
 const totalWeeklyCost = fCost + hCost + tCost + oCost;
 
 const difference = wIncome - totalWeeklyCost;
 
 if (difference < weeklySavings) {
-  alert(`you need to save ${Math.round(weeklySavings - difference)} extra!`);
+  alert(`you need to save ${Math.round(weeklySavings - difference).toFixed(2)} extra!`);
 } else {
   alert("your're on the right track!");
 }
